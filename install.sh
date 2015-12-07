081672cb54c660044156672bbfdc87884a801f69
Alexander Biskop <abiskop@users.noreply.github.com>
2015-12-07 12:09:56 +0100

    upgraded to node v0.12.9

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-NODE_VERSION=0.12.7
+NODE_VERSION=0.12.9
 curl -sL "http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz" > "/tmp/node-v$NODE_VERSION-linux-x64.tar.gz"
 tar xvzf "/tmp/node-v$NODE_VERSION-linux-x64.tar.gz" --strip-components=1 --no-same-owner -C "/usr/local"
 echo "export NODE_PATH=/usr/local/lib/node_modules" >> ~/.bashrc
