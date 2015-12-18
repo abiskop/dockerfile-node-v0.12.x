90a3e2b4d8dee9d6f9145fb8045f3a66f750d7ef
Alexander Biskop <abiskop@users.noreply.github.com>
2015-12-18 12:27:48 +0100

    upgrade npm after installing node

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@ NODE_VERSION=0.12.9
 curl -sL "http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz" > "/tmp/node-v$NODE_VERSION-linux-x64.tar.gz"
 tar xvzf "/tmp/node-v$NODE_VERSION-linux-x64.tar.gz" --strip-components=1 --no-same-owner -C "/usr/local"
 echo "export NODE_PATH=/usr/local/lib/node_modules" >> ~/.bashrc
+echo "node version:"
 node --version
-
+echo "npm version:"
+npm --version
+echo "Upgrading npm"
+npm install npm -g
+echo "npm version:"
+npm --version
